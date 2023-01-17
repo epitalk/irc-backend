2d9f9a977d3c846bb9325f544e02366e81f516af
Coco <corentin.recanzone@epitech.eu>
2023-01-17 15:19:58 +0100

    fix: conflicts

--- start/routes.ts
+++ start/routes.ts
@@ -3,7 +3,7 @@ import PostsController from 'App/Controllers/Http/PostsController'
 import './routes/sse-starter.ts'
 
 Route.get('/', async () => {
-  return { hello: 'DSDS' }
+  return { hello: 'xxxx' }
 })
 
 Route.get('/test', async (ctx) => {
