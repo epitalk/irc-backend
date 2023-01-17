72999db2431889ea0fc31dcc9ee0fdd02c3ab4a0
Léo Guillaume <leo.guillaume@epitech.eu>
2023-01-17 12:38:41 +0100

    add sse starter routes

---
+++ start/routes/sse-starter.ts
@@ -0,0 +1,59 @@
+import Route from '@ioc:Adonis/Core/Route'
+import jws from 'jws'
+import got from 'got'
+
+Route.get('/auth/:id', async () => {
+  return await computedToken({
+    mercure: { subscribe: [`/alert`] },
+  })
+})
+
+Route.post('/chat/public', async ({ request }) => {
+  const message: string = await request.input('message')
+
+  new Update(['/chat'], { message }).send()
+
+  return { message }
+})
+
+Route.get('/private', () => {
+  const data = { foo: 'bar' }
+
+  new Update(['/chat'], data, true).send()
+
+  return { status: 'Ok' }
+})
+
+class Update {
+  constructor(
+    private topics: string[],
+    private data: Record<string, string> = {},
+    private isPrivate: boolean = false
+  ) {}
+
+  public send() {
+    return got.post('http://localhost:1405/.well-known/mercure', {
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InB1Ymxpc2giOlsiKiJdfX0.obDjwCgqtPuIvwBlTxUEmibbBf0zypKCNzNKP7Op2UM',
+      },
+      form: [
+        ...this.topics.map((topic) => ['topic', topic]),
+        ['data', JSON.stringify(this.data)],
+      ].concat(this.isPrivate ? ['private', 'on'] : []),
+    })
+  }
+}
+
+function computedToken(payload) {
+  return new Promise((resolve, reject) => {
+    jws
+      .createSign({
+        payload,
+        secret: '!ChangeMe!',
+        header: { alg: 'HS256' },
+      })
+      .on('error', reject)
+      .on('done', resolve)
+  })
+}
