1325132aa765a287f52f859db1fe374bace3f3d6
Coco <corentin.recanzone@epitech.eu>
2023-01-23 17:06:55 +0100

    m

--- start/routes.ts
+++ start/routes.ts
@@ -1,4 +1,9 @@
+import Route from '@ioc:Adonis/Core/Route'
 import './routes/sse-starter'
 import './routes/message'
 import './routes/channel'
 import './routes/user'
+
+Route.get('/', () => {
+  return { test: 'ok' }
+})
