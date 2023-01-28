c79e5ec5b76910272931ab3e67cb103f0d2cbb75
Léo Guillaume <leo.guillaume@gmail.com>
2023-01-28 17:30:40 +0100

    fix: add delete cascade on channel user table migration

--- database/migrations/1674656090901_channel_user.ts
+++ database/migrations/1674656090901_channel_user.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('channel_id').unsigned().references('channel.id')
-      table.integer('user_id').unsigned().references('user.id')
+      table.integer('channel_id').unsigned().references('channel.id').onDelete('CASCADE')
+      table.integer('user_id').unsigned().references('user.id').onDelete('CASCADE')
       table.unique(['channel_id', 'user_id'])
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
